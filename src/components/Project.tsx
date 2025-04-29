cba2ff62bd10dba93c5e365e1a98ad1c07a1fd4a
Emanuele Czofei <emanuelecz06@gmail.com>
2025-04-29 15:33:56 +0200

    TypeScript-Portfolio

---
+++ src/components/Project.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+
+interface Project {
+  imgUrl:string,
+  title:string,
+  description:string,
+  projectUrl:string,
+  skills: string[]
+}
+
+const Project: React.FC<Project> = ({ imgUrl, title, description, projectUrl, skills }) => {
+  return (
+    <div className="w-full bg-[#3d3d3d] rounded-[8px] p-4 mb-4 flex flex-col sm:flex-row">
+      {/* Image */}
+      <img
+        src={imgUrl}
+        alt="project image"
+        className="sm:w-[150px] md:w-[200px]  rounded-[8px] cursor-pointer mb-4 sm:mb-0 sm:mr-4 self-center"
+      />
+
+      {/* Text content */}
+      <div className="flex flex-col justify-start">
+        <h1 className="text-lg md:text-2xl text-white underline mb-2">
+          <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h1>
+        <p className="text-md md:text-[17px] text-white mb-2">{description}</p>
+        
+        {/* Skills */}
+        <div className="flex flex-wrap">
+          {skills.map((skill:string, index:number) => (
+            <span
+              key={index}
+              className="bg-[#1f1f1f] text-white text-xs md:text-sm px-3 py-1 rounded-full mr-2 mb-2"
+            >
+              {skill}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
